b64a2103f42bc4adf7b1ac6cbb3d84a38597a8f3
yamas1991 <ec2-user@ip-172-31-79-85.ec2.internal>
2021-07-17 11:36:52 +0000

    BOOKERS

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      #書き込み失敗
+      @books = Book.all
+      render action: :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      #書き込み失敗
+      render action: :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
